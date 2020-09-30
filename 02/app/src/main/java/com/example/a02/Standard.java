6d39776d5d6776813abeaee0336447eb1d648803
DESKTOP-DUJV48R\start <2945915066@qq.com>
2020-09-30 22:19:57 +0800

    提交文件2

---
+++ 02/app/src/main/java/com/example/a02/Standard.java
@@ -0,0 +1,27 @@
+package com.example.a02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Standard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("Standard",this.toString());
+        setContentView(R.layout.activity_standard);
+        Button button1=(Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Standard.this,Standard.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
