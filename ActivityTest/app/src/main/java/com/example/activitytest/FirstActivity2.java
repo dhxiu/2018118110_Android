2e04c97b1ccf640e2959e51aedc0a7cb46beee6d
DESKTOP-DUJV48R\start <2945915066@qq.com>
2020-09-30 22:06:37 +0800

    提交本次作业

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity2.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity2.java
@@ -1,16 +1,18 @@
 package com.example.activitytest;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity2 extends AppCompatActivity {
 
@@ -36,15 +38,19 @@ public class FirstActivity2 extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("FirstActivity",this.toString());
         setContentView(R.layout.activity_first2);
         Button button1=(Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(FirstActivity2.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(FirstActivity2.this,FirstActivity2.class);
+                startActivity(intent);
+
             }
         });
     }
