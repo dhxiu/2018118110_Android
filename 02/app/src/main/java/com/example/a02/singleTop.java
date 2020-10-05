8ad80bd197d7f65acc869761aa85d1f26bc9f76f
DESKTOP-DUJV48R\start <2945915066@qq.com>
2020-10-05 09:01:50 +0800

    提交single Top模式

--- 02/app/src/main/java/com/example/a02/singleTop.java
+++ 02/app/src/main/java/com/example/a02/singleTop.java
@@ -8,18 +8,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class Standard extends AppCompatActivity {
+public class singleTop extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Standard",this.toString());
-        setContentView(R.layout.activity_standard);
-        Button button1=(Button) findViewById(R.id.button_1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Log.d("singTop",this.toString());
+        setContentView(R.layout.activity_single_top);
+        Button button2=(Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Standard.this,singleTop.class);
+                Intent intent=new Intent(singleTop.this,Standard.class);
                 startActivity(intent);
             }
         });
