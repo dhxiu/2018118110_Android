077f5fec2450b5b70f5ba24822b63003d0599cda
DESKTOP-DUJV48R\start <2945915066@qq.com>
2020-10-11 22:55:13 +0800

    提交

--- 03/app/src/main/java/com/example/myapplication/firstActivity.java
+++ 03/app/src/main/java/com/example/myapplication/firstActivity.java
@@ -9,6 +9,7 @@ public class firstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_first);
         setContentView(R.layout.activity_first);
     }
 }
