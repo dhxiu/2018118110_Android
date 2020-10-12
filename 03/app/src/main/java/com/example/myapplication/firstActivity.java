ff32c00bb81087f0e4cc98d30736681a20fcc26f
DESKTOP-DUJV48R\start <2945915066@qq.com>
2020-10-12 13:42:35 +0800

    提交ImageView

--- 03/app/src/main/java/com/example/myapplication/firstActivity.java
+++ 03/app/src/main/java/com/example/myapplication/firstActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class firstActivity<finishActivity> extends AppCompatActivity implements View.OnClickListener{
     private EditText editText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class firstActivity<finishActivity> extends AppCompatActivity implements 
         setContentView(R.layout.activity_first);
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
         button.setOnClickListener((View.OnClickListener) this);
     }
 
@@ -25,6 +28,7 @@ public class firstActivity<finishActivity> extends AppCompatActivity implements 
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.button:
+                imageView.setImageResource(R.drawable.tiger);
                 String inputText = editText.getText().toString();
                 Toast.makeText(firstActivity.this,inputText,Toast.LENGTH_SHORT).show();
                 break;
