14f2d43ee0b15371f20b2234ec0ee98adbd494ba
dhxiu <2945915066@qq.com>
2020-12-13 00:00:12 +0800

    服务

---
+++ QZH3/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.servicetest;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startService = (Button) findViewById(R.id.start_service);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_service:
+        }
+    }
+}
